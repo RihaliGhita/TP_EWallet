43f7ffe85c4dfceb9d048777823e0258b7c85d88
RihaliGhita <rihalighita@gmail.com>
2025-11-17 20:44:55 +0100

    Update ComptePayPal.h

--- TP_EWallet/ComptePayPal.h
+++ TP_EWallet/ComptePayPal.h
@@ -1,19 +1,15 @@
 #pragma once
 #include "MoyenPaiement.h"
+#include"Rechargeable.h"
 class ComptePayPal : public MoyenPaiement,public Rechargeable
 {
 private:
-	static int compteur;
-	int id;
 	string adresse_email;
 	double solde;
 public:
 	ComptePayPal(string,string,double);
 	bool payer(double montant) override;
 	void recharger(double montant)override;
-	bool authentifier()override;
 	void afficher()const override;
-
-	
 };
 
